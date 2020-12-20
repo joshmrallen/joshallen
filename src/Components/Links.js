9189df992effe21481f76512e8fbd7e03fc1f68e
github username <joshmrallen@gmail.com>
2020-12-20 11:13:54 -0500

    got image in about links working and styled

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Octocat from '../images/GitHub-Mark-Light-120px-plus.png'
 
 const Links = () => {
 
     return(
         <div>
             <ul>
-                <li><img src="../images/GitHub-Mark-Light-120px-plus.png" /></li>
+                <li>
+                    <div>
+                        <img className="link-image" src={Octocat} alt="github mark image" />
+                    </div>
+                </li>
             </ul>
         </div>
     )
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
         <ul className="navbar">
             <li><NavLink exact={true} to="/" className="navlink" activeClassName="active-link">HOME</NavLink></li>
             <li><NavLink to="/about" className="navlink" activeClassName="active-link">ABOUT</NavLink></li>
-            <li><NavLink to="/portfolio" className="navlink" activeClassName="active-link">PORTFOLIO</NavLink></li>
+            <li><NavLink to="/portfolio" className="navlink" activeClassName="active-link">PROJECTS</NavLink></li>
             <li><NavLink to="/blog" className="navlink" activeClassName="active-link">BLOG</NavLink></li>
             <li><NavLink to="/contact" className="navlink" activeClassName="active-link">CONTACT</NavLink></li>
         </ul>
