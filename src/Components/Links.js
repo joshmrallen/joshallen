36f95e3a2c83ecac38833d4b9b0c7bf136cc4d20
github username <joshmrallen@gmail.com>
2020-12-20 23:50:28 -0500

    added social logos under name for about section

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Octocat from '../images/GitHub-Mark-Light-120px-plus.png'
+import LinkedIn from '../images/LI-In-Bug.png'
+import Medium from '../images/Medium-Symbol-Black-RGB@4x.png'
+import Twitter from '../images/Twitter_Logo_WhiteOnImage.png'
 
 const Links = () => {
 
@@ -7,9 +10,16 @@ const Links = () => {
         <div>
             <ul>
                 <li>
-                    <div>
-                        <img className="link-image" src={Octocat} alt="github mark image" />
-                    </div>
+                    <img className="link-image" src={Octocat} alt="github mark image" />
+                </li>
+                <li>
+                    <img className="link-image" src={LinkedIn} alt="LinkedIn 'In' logo" />
+                </li>
+                <li>
+                    <img className="link-image" src={Medium} alt="medium blog symbol" />
+                </li>
+                <li>
+                    <img className="link-image" src={Twitter} alt="twitter logo" />
                 </li>
             </ul>
         </div>
